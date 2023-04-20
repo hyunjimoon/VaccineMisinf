4d6a83a8b2acaca2efb816a2ab16cac20b930c40
Angie.H Moon <30194633+hyunjimoon@users.noreply.github.com>
2023-04-20 18:04:09 -0400

    Update vac_cost_eff.py

--- V79/vac_cost_eff.py
+++ V79/vac_cost_eff.py
@@ -4,6 +4,7 @@ from VST.vst import vst_text, vst
 import pandas
 import os
 import json
+import xarray as xr
 FINAL_TIME=1100
 vgpath = "C:/Program Files (x86)/Vengine2/Vensim - vengine.exe"
 log = "log.txt"
@@ -20,10 +21,10 @@ state_lst_4=[]
 state_lst_5=[]
 state_lst_6=[]
 for name in state_names:
-    # state_lst_1.append(f"Impact of Treatment on Fatality Rate[{name}]")
-    # state_lst_2.append(f"Base Fatality Rate for Unit Acuity[{name}]")
-    # state_lst_3.append(f"Reference Force of Infection[{name}]")
-    # state_lst_4.append(f"Strength of Adherence Fatigue[{name}]")
+    state_lst_1.append(f"Impact of Treatment on Fatality Rate[{name}]")
+    state_lst_2.append(f"Base Fatality Rate for Unit Acuity[{name}]")
+    state_lst_3.append(f"Reference Force of Infection[{name}]")
+    state_lst_4.append(f"Strength of Adherence Fatigue[{name}]")
     state_lst_5.append(f"Variant Intro Start Time[{name}]")
     state_lst_6.append(f"Variant Intro Start Time2[{name}]")
 
@@ -32,22 +33,56 @@ param_lst_lst=[
     # state_lst_2,
     # state_lst_3,
     # state_lst_4,
-    state_lst_5,
+    # state_lst_5,
     state_lst_6,
-    ['Variant Impact on Immunity Loss Time[Omicron]'],
-    ['Variant Impact on Immunity Loss Time[Delta]'],
-    ['Variant Impact on Immunity Loss Time[BA5]'],
-    ['Extra Vaccine Impact on Responsiveness[Vx]'],
-    ['Extra Vaccine Impact on Responsiveness[NVx]'],
-    ['Extra Vaccine Impact on Responsiveness[Naive]'],
-    ['Immunity Loss Time[Vx]'],
-    ['Immunity Loss Time[NVx]'],
-    ['Immunity Loss Time[Naive]'],
-    ['Variant Accuity Multiplier[Omicron]'],
-    ['Variant Accuity Multiplier[Delta]'],
-    ['Variant Accuity Multiplier[BA5]'],
-    ['Variant Intro Start Time3'],
- ['Variant Transmission Multiplier[Omicron]'], ['Variant Transmission Multiplier[Delta]'], ['Variant Transmission Multiplier[BA5]']]
+    # ['Variant Impact on Immunity Loss Time[Omicron]'],
+    # ['Variant Impact on Immunity Loss Time[Delta]'],
+    # ['Variant Impact on Immunity Loss Time[BA5]'],
+    # ['Extra Vaccine Impact on Responsiveness[Vx]'],
+    # ['Extra Vaccine Impact on Responsiveness[NVx]'],
+    # ['Extra Vaccine Impact on Responsiveness[Naive]'],
+    # ['Immunity Loss Time[Vx]'],
+    # ['Immunity Loss Time[NVx]'],
+    # ['Immunity Loss Time[Naive]'],
+    # ['Variant Accuity Multiplier[Omicron]'],
+    # ['Variant Accuity Multiplier[Delta]'],
+    # ['Variant Accuity Multiplier[BA5]'],
+    # ['Variant Intro Start Time3'],
+    # ['Variant Transmission Multiplier[Omicron]'],
+    # ['Variant Transmission Multiplier[Delta]'],
+    # ['Variant Transmission Multiplier[BA5]']
+]
+vax_inc_1dot1=1.01
+vax_inc_base=1
+vax_inc_zero=0
+TIME_PERIODS=89#1071
+x=pd.date_range(start = '10/15/2019', periods = TIME_PERIODS)
+dv = {
+    "coords": {
+        'value': {"dims": ("value"), "data": ["marginal", "average", str(vax_inc_1dot1), str(vax_inc_base), str(vax_inc_zero)]},
+        'component': {"dims": ("component"),  "data": ['death', 'gdp', 'hospitalization']},
+        'params': {"dims": ("params"), "data": [param[0] for param in param_lst_lst]+["baseline"]},
+        'space': {"dims" : ("space"), "data": state_names},
+        'time': {"dims" : ("time"), "data": pd.date_range(start = '10/15/2019', periods = TIME_PERIODS)}
+    },
+    "dims": {"value", "component", "params"},
+    "data_vars":{ #5+ 3
+        'VV_cstv': {"dims": ("component","space", "value", "time"), "data": np.zeros(shape = (3, len(state_names), 5, TIME_PERIODS))},
+        'VV_ctv': {"dims": ("component", "value", "time"), "data": np.zeros(shape = (3, 5, TIME_PERIODS))},
+        'VV_stv': {"dims": ("space", "value", "time"), "data": np.zeros(shape = (len(state_names), 5, TIME_PERIODS))},
+        'VV_tv': {"dims": ("value", "time"), "data": np.zeros(shape = (5, TIME_PERIODS))},
+
+        'VV_1_pcstv': {"dims": ("params", "component", "space", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1,3,len(state_names), 5, TIME_PERIODS))},
+        'VV_1_pctv': {"dims": ("params", "component", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1,3, 5, TIME_PERIODS))},
+        'VV_1_ptv': {"dims": ("params", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1, 5, TIME_PERIODS))},
+
+        'epsilon_ptv': {"dims": ("params", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1, 5, TIME_PERIODS))},
+        'epsilon_pctv': {"dims": ("params", "component", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1, 3, 5, TIME_PERIODS))},
+        'epsilon_pcstv': {"dims": ("params", "component", "space", "value", "time"), "data": np.zeros(shape = (len(param_lst_lst)+1, 3, len(state_names), 5, TIME_PERIODS))},
+    },
+   "attrs": {"title": "vaccine value disaggregated by parameter, time, space, component"}
+    }
+vds = xr.Dataset.from_dict(dv)
 
 controlfilename = 'Control.txt' #input("Enter control file name (with extension):")
 cf = json.load(open(controlfilename, 'r'))
@@ -55,24 +90,22 @@ cf = json.load(open(controlfilename, 'r'))
 for k,v in cf.items():
     exec(k + '=v')
 
-vax_inc_1dot1=1.01
-vax_inc_base=1
-vax_inc_zero=0
-def calc_change_with_respect_to_pop(change):
-    cost_per_person=np.zeros(shape=change.shape)
-    cost_per_person[0]=change[0]/change[3] #death
-    cost_per_person[1]=change[1]/change[3]# gdp
-    cost_per_person[2]=change[2]/change[3] #hospitalization
-    cost_per_person[3]=(change[0]+change[1]+change[2])/change[3]
-    for i in range(change.shape[0]-51, change.shape[0]):
-        cost_per_person[i-51*3]=change[i-51*3]/change[i]#4
-        cost_per_person[i-51*2]=change[i-51*2]/change[i]#55
-        cost_per_person[i-51*1]=change[i-51*1]/change[i]#106
-        cost_per_person[i]=(change[i-51*1]+change[i-51*2]+change[i-51*3])/change[i]#157
-    print(cost_per_person)
-
-    return cost_per_person
-def calc_param_impact_on_cost(param_name_list, percent_change): #list of str or list of list (local variables), # real
+def calc_change_with_respect_to_pop(raw_values: pandas.DataFrame) -> pandas.DataFrame:
+    values_normalized_by_pop=pandas.DataFrame()
+    columns_to_adjust=[
+        "Cost of Deaths Cumulative",
+        "Cost of GDP Cumulative",
+        "Cost of Hospitalizations Cumulative"
+    ]
+    population_column = "Vaccine Taking Population"
+    locations = [f"[{state}]" for state in state_names]
+    locations+=[" All"]
+    for location in locations:
+        for column in columns_to_adjust:
+            values_normalized_by_pop[column+location]=raw_values[column+location]/raw_values[population_column+location]
+    print(values_normalized_by_pop)
+    return values_normalized_by_pop
+def calc_param_impact_on_cost(param_name_list, percent_change, storage_xarr, use_vengine=False, param_name=None): #list of str or list of list (local variables), # real
     """Find the costs of the pandemic at vaxinc vals of 0, 1, 1.01 when the params passed in are changed by percent_change
         with base parameter
         b1. calc the cost difference VaxInc: diff_cost_01, diff_cost_1_1dot1
@@ -84,61 +117,112 @@ def calc_param_impact_on_cost(param_name_list, percent_change): #list of str or 
         c3. marginal cost: mc_01_c = diff_cost_01_c / diff_ppl_01_c, mc_1_1dot1_c = diff_cost_1_1dot1_c/diff_ppl_1_1dot1_c
         ela_01 = ((mc_01_c - mc_01) / mc_01) / 0.01
         ela_1_1dot1 = ((mc_1_1dot1_c - mc_1_1dot1) / mc_1_1dot1) / 0.01 #not doing this right now"""
-    full_output_df=pd.DataFrame()
+    if not param_name:
+        param_name=param_name_list[0]
     for VaxInc_val in [0, 1, 1.01]:
-        param_new_val_list=[]
-        for item in param_name_list:
-            try:
-                param_base = vst_text.get_value("V79-OptimTest.out", item)
-            except TypeError: #if we can't find param in optimization output, pull it out of model directly, should be done form mdl but is currently from .tab generated from vensim
-                df = pandas.read_csv("test.tab", sep="\t", index_col=0) #raw values (all params outside .out, manually generated from .mdl/ can read from .mdl)
-                param_base=df.loc[item][0]
-            param_new_val_list.append(param_base * percent_change)  # TODO: make this function with list param for rgn
-        setval_lst = [("VaxInc", VaxInc_val)]+[(param_name, param_new_val) for param_name, param_new_val in zip(param_name_list, param_new_val_list)]
-        run = vst.Script(cf, f"param_{param_name_list[0].split()[0]}", log, setvals=setval_lst, simtype='r')
-        run.compile_script(vgpath, log, subdir="VAMA", check_funcs=[lambda a, b: True])
-        output_df = pandas.read_csv(f"VAMA/Covidparam_{param_name_list[0].split()[0]}.csv")
-        output_vals = output_df[output_df.columns[-1]] # removing time dims # multi-index
-        output_vals.rename(VaxInc_val, inplace=True)
-        full_output_df = full_output_df.append(
-            output_vals)
-    return full_output_df.T
+        if use_vengine:
+            param_new_val_list=[]
+            for item in param_name_list:
+                try:
+                    param_base = vst_text.get_value("V79-OptimTest.out", item)
+                except TypeError: #if we can't find param in optimization output, pull it out of model directly, should be done form mdl but is currently from .tab generated from vensim
+                    df = pandas.read_csv("test.tab", sep="\t", index_col=0) #raw values (all params outside .out, manually generated from .mdl/ can read from .mdl)
+                    param_base=df.loc[item][0]
+                param_new_val_list.append(param_base * percent_change)  # TODO: make this function with list param for rgn
+            setval_lst = [("VaxInc", VaxInc_val)]+[(param_name, param_new_val) for param_name, param_new_val in zip(param_name_list, param_new_val_list)]
+            run = vst.Script(cf, f"param_{param_name.split()[0]}", log, setvals=setval_lst, simtype='r')
+            run.compile_script(vgpath, log, subdir="VAMA", check_funcs=[lambda a, b: True])
+        param_vv = pandas.read_csv(f"VAMA/Covidparam_{param_name.split()[0]}.csv", index_col=0).T
+        param_vv = param_vv.iloc[0:89]
+        param_vv = calc_change_with_respect_to_pop(param_vv)
+
+        storage_xarr["VV_1_pctv"].loc[{
+            "params":param_name,
+            "component": "death",
+            "value": str(VaxInc_val)
+        }] = param_vv["Cost of Deaths Cumulative All"].values.flatten()
+        storage_xarr["VV_1_pctv"].loc[{
+            "params":param_name,
+            "component": "gdp",
+            "value": str(VaxInc_val)
+        }] = param_vv["Cost of GDP Cumulative All"].values.flatten()
+        storage_xarr["VV_1_pctv"].loc[{
+            "params":param_name,
+            "component": "hospitalization",
+            "value": str(VaxInc_val)
+        }] = param_vv["Cost of Hospitalizations Cumulative All"].values.flatten()
+        for state in state_names:
+            storage_xarr["VV_1_pcstv"].loc[{
+                "params":param_name,
+                "component": "death",
+                "value": str(VaxInc_val),
+                "space": state
+            }] = param_vv[f"Cost of Deaths Cumulative[{state}]"].values.flatten()
+            storage_xarr["VV_1_pcstv"].loc[{
+                "params":param_name,
+                "component": "gdp",
+                "value": str(VaxInc_val),
+                "space": state
+            }] = param_vv[f"Cost of GDP Cumulative[{state}]"].values.flatten()
+            storage_xarr["VV_1_pcstv"].loc[{
+                "params":param_name,
+                "component": "hospitalization",
+                "value": str(VaxInc_val),
+                "space": state
+            }] = param_vv[f"Cost of Hospitalizations Cumulative[{state}]"].values.flatten()
+
+    storage_xarr["VV_1_pcstv"].loc[{
+        "value": "average"
+    }] = storage_xarr["VV_1_pcstv"].loc[{
+        "value": str(vax_inc_base)
+    }] - storage_xarr["VV_1_pcstv"].loc[{
+        "value": str(vax_inc_zero)
+    }]
+    
+    storage_xarr["VV_1_pcstv"].loc[{
+        "value": "marginal"
+    }] = storage_xarr["VV_1_pcstv"].loc[{
+        "value": str(vax_inc_1dot1)
+    }] - storage_xarr["VV_1_pcstv"].loc[{
+        "value": str(vax_inc_base)
+    }]
+
+    storage_xarr["VV_1_pstv"] = storage_xarr["VV_1_pcstv"].sum(dim="component")
+    storage_xarr["VV_1_ptv"] = storage_xarr["VV_1_pctv"].sum(dim="component")
 # baseline run, the name sensitivity to weather is chosen arbritraily
-results=calc_param_impact_on_cost(["Sensitivity to Weather"], 1) #dataframe
-marg_delta_wrt_vaxinc=results[vax_inc_1dot1]-results[vax_inc_base] # $
-marg_cost_per_person=calc_change_with_respect_to_pop(marg_delta_wrt_vaxinc) #$/person
-
-avg_delta_wrt_vaxinc=results[vax_inc_base]-results[vax_inc_zero] # not df.iloc[1] but df.loc[1] as the col name is int (0,1,1.01)
-avg_cost_per_person=calc_change_with_respect_to_pop(avg_delta_wrt_vaxinc)
-
-pd.Series(marg_cost_per_person).to_csv("marg_cost_per_person_base.tsv", sep="\t")
-pd.Series(avg_cost_per_person).to_csv("avg_cost_per_person_base.tsv", sep="\t")
-marg_elasticity_dict={}
-avg_elas_dict={}
+results=calc_param_impact_on_cost(["Sensitivity to Weather"], 1, vds, use_vengine=False, param_name="baseline") # baseline
 for param_lst in param_lst_lst:
     print(param_lst)
-    results_param = calc_param_impact_on_cost(param_lst, 1.01)# $
+    calc_param_impact_on_cost(param_lst, 1.01, vds, use_vengine=False)
+    vds["epsilon_ptv"]=((vds["VV_1_ptv"].loc[{"params":param_lst[0]}]-vds["VV_1_ptv"].loc[{"params":"baseline"}])/vds["VV_1_ptv"].loc[{"params":"baseline"}])/0.01
 
-    marg_diff = results_param[vax_inc_1dot1] - results_param[vax_inc_base] #$
-    marg_cost_per_person_param1dot1=calc_change_with_respect_to_pop(marg_diff) # $/person
+print(vds)
 
-    avg_diff = results_param[vax_inc_base] - results_param[vax_inc_zero]
-    avg_cost_per_person_param1dot1=calc_change_with_respect_to_pop(avg_diff)
+import matplotlib
 
-    marg_elasticity_dict[param_lst[0]]= ((marg_cost_per_person_param1dot1 - marg_cost_per_person) / marg_cost_per_person) / (0.01) # unitless
-    avg_elas_dict[param_lst[0]]= ((avg_cost_per_person_param1dot1 - avg_cost_per_person) / avg_cost_per_person) / (0.01)
+def plot_sensitivity(vds):
+    matplotlib.pyplot.rcdefaults()
+    fig_avg, ax_avg = matplotlib.pyplot.subplots()
+    fig_marg, ax_marg = matplotlib.pyplot.subplots()
 
-    print(marg_elasticity_dict[param_lst[0]]) # giving nan bc of division by zero
-    print(avg_elas_dict[param_lst[0]])
-    filename = filename.replace("[Alabama]", "")
-    filename = f"{param_lst[0].replace(' ','_')}_marg.tsv"
-    pd.Series(marg_elasticity_dict[param_lst[0]]).to_csv(filename, sep ="\t") # giving nan bc of division by zero
-    filename = f"{param_lst[0].replace(' ', '_')}_avg.tsv"
-    pd.Series(avg_elas_dict[param_lst[0]]).to_csv(filename, sep ="\t")
+    x_avg=vds['VV_tv'].loc[{"value":"average"}]
+    x_marg=vds['VV_tv'].loc[{"value":"marginal"}]
+    x_label=[val for val in vds.params.values]
+    y_vars=[x for x in range(len(vds['VV_1_ptv']))]
 
-# stores the list of params in tsv file
-params = [x[0].replace("[Alabama]", "") for x in param_lst_lst]
-file = open('params.txt','w')
-for item in params:
-	file.write(item+"\n")
-file.close()
+    ax_avg.barh(x_label_sorted, x_avg_sorted, align='center')
+    ax_avg.set_yticks(y_vars)
+    ax_avg.invert_yaxis()  # labels read top-to-bottom
+    ax_avg.set_xlabel('Average Vaccine Cost Efficacy')
+    ax_avg.set_title('Elasticity of Average Vaccine Cost Efficacy')
+
+    ax_marg.barh(x_label_sorted, x_marg_sorted, align='center')
+    ax_marg.set_yticks(y_vars)
+    ax_marg.invert_yaxis()  # labels read top-to-bottom
+    ax_marg.set_xlabel('Marginal Vaccine Cost Efficacy')
+    ax_marg.set_title('Elasticity of Marginal Vaccine Cost Efficacy')
+
+    matplotlib.pyplot.show()
+    return
+
+plot_sensitivity(vds)
